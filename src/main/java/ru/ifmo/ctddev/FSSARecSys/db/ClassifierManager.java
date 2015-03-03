d9cb81c1a3fed28138db2d3ed4f7d8fe737c8e54
Undin <arseniy1993@mail.ru>
2015-03-03 21:46:01 +0300

    added docs

--- src/main/java/ru/ifmo/ctddev/FSSARecSys/db/ClassifierManager.java
+++ src/main/java/ru/ifmo/ctddev/FSSARecSys/db/ClassifierManager.java
@@ -10,8 +10,34 @@ public interface ClassifierManager {
 
     public ClassifierEvaluator get(String name);
 
+    /**
+     * @see #register(String, String, String[], java.io.File)
+     */
     public boolean register(String name, String className);
-    public boolean register(String name, String className, String[] arguments);
+
+    /**
+     * @param name classifier's name
+     * @param className the fully qualified class name of the classifier which extends <code>{@link weka.classifiers.Classifier}</code>.
+     *                  <code>{@link weka.classifiers.Classifier#forName(String, String[])}</code> method is used to create classifier object.
+     * @param options an array of options suitable for passing to <code>{@link weka.classifiers.Classifier#forName(String, String[])}</code>.
+     *                  May be null.
+     * @return <tt>true</tt> if classifier was successfully registered, <tt>false</tt> otherwise
+     */
+    public boolean register(String name, String className, String[] options);
+
+    /**
+     * @see #register(String, String, String[], java.io.File)
+     */
     public boolean register(String name, String className, File jarFile);
-    public boolean register(String name, String className, String[] arguments, File jarFile);
+
+    /**
+     * @param name classifier's name
+     * @param className the fully qualified class name of the classifier which extends <code>{@link weka.classifiers.Classifier}</code>.
+     *                  <code>{@link weka.classifiers.Classifier#forName(String, String[])}</code> method is used to create classifier object.
+     * @param options an array of options suitable for passing to <code>{@link weka.classifiers.Classifier#forName(String, String[])}</code>.
+     *                  May be null.
+     * @param jarFile file with compiled code of classifier
+     * @return <tt>true</tt> if classifier was successfully registered, <tt>false</tt> otherwise
+     */
+    public boolean register(String name, String className, String[] options, File jarFile);
 }
