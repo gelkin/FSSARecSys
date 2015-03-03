b78c2f74af347b9bab70b3e2144109346bcdad72
Undin <arseniy1993@mail.ru>
2015-03-03 18:27:09 +0300

    implement averageClassifierResult() method

--- src/main/java/ru/ifmo/ctddev/FSSARecSys/ClassifierResult.java
+++ src/main/java/ru/ifmo/ctddev/FSSARecSys/ClassifierResult.java
@@ -1,14 +1,33 @@
 package ru.ifmo.ctddev.FSSARecSys;
 
 
+import java.util.List;
+
 public class ClassifierResult {
     /**
      * Accuracy computed on given DataSet with given Classifier
      */
-    public float accuracy;
+    public double accuracy;
 
     /**
      * F1 measure computed on given DataSet with given Classifier
      */
-    public float F1Measure;
+    public double f1Measure;
+
+    public ClassifierResult(double accuracy, double f1Measure) {
+        this.accuracy = accuracy;
+        this.f1Measure = f1Measure;
+    }
+
+    public static ClassifierResult averageClassifierResult(List<ClassifierResult> results) {
+        double averageAccuracy = 0;
+        double averageF1Measure = 0;
+        for (ClassifierResult result : results) {
+            averageAccuracy += result.accuracy;
+            averageF1Measure += result.f1Measure;
+        }
+        averageAccuracy /= results.size();
+        averageF1Measure /= results.size();
+        return new ClassifierResult(averageAccuracy, averageF1Measure);
+    }
 }
