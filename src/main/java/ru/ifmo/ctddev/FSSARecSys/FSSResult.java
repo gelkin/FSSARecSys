c2d2981df3116726c4b6ed393ca32084f3f692c9
Undin <arseniy1993@mail.ru>
2015-03-04 14:54:11 +0300

    added dataSetName and algorithmName to FSSResult struct. created conctructor

--- src/main/java/ru/ifmo/ctddev/FSSARecSys/FSSResult.java
+++ src/main/java/ru/ifmo/ctddev/FSSARecSys/FSSResult.java
@@ -1,7 +1,21 @@
 package ru.ifmo.ctddev.FSSARecSys;
 
+import java.util.Objects;
+
 public class FSSResult {
+
+    public String dataSetName;
+    public String algorithmName;
+
     public int numberOfSelectedFeatures;
-    public double selectionTime;
     public int[] selectedFeatures;
+    public long selectionTime;
+
+    public FSSResult(String dataSetName, String algorithmName, int[] selectedFeatures, long selectionTime) {
+        this.dataSetName = Objects.requireNonNull(dataSetName);
+        this.algorithmName = Objects.requireNonNull(algorithmName);
+        this.selectedFeatures = Objects.requireNonNull(selectedFeatures);
+        this.numberOfSelectedFeatures = selectedFeatures.length;
+        this.selectionTime = selectionTime;
+    }
 }
