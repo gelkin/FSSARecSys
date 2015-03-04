89e40dd5cd4fd4665603880bdc3e489b8dfa7671
Undin <arseniy1993@mail.ru>
2015-03-04 18:49:59 +0300

    added default implementation of FSSAlgorithmManager.get(List<String>)

--- src/main/java/ru/ifmo/ctddev/FSSARecSys/db/FSSAlgorithmManager.java
+++ src/main/java/ru/ifmo/ctddev/FSSARecSys/db/FSSAlgorithmManager.java
@@ -3,13 +3,24 @@ package ru.ifmo.ctddev.FSSARecSys.db;
 import ru.ifmo.ctddev.FSSARecSys.calculators.FSSAlgorithm;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
 
 public interface FSSAlgorithmManager {
     public List<String> getAvailableFSSAlgorithms();
 
     public FSSAlgorithm get(String name);
-    public List<FSSAlgorithm> get(List<String> names);
+
+    public default List<FSSAlgorithm> get(List<String> names) {
+        List<FSSAlgorithm> algorithms = new ArrayList<>();
+        for (String name : names) {
+            FSSAlgorithm algorithm = get(name);
+            if (algorithm != null) {
+                algorithms.add(algorithm);
+            }
+        }
+        return algorithms;
+    }
 
     public boolean register(String name, String algorithmClassName, String evaluationClassName);
     public boolean register(String name, String algorithmClassName, String[] algorithmOptions, String evaluationClassName, String[] evaluationOptions);
