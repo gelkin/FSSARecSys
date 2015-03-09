5092ae7bff255992e6c3632858e459939387489d
Igor Tanfilyev <tanfilyev@gmail.com>
2015-03-10 01:46:24 +0300

    delete temporary created interface

--- src/main/java/ru/ifmo/ctddev/FSSARecSys/db/MetaFeaturesInterface.java
+++
@@ -1,8 +0,0 @@
-package ru.ifmo.ctddev.FSSARecSys.db;
-
-public interface MetaFeaturesInterface {
-    public double getMetaFeature(String name);
-    public double[] getMetaFeatures(String[] names);
-    public void setMetaFeature(String name, double value);
-    public void setMetaFeatures(String[] names, double[] values);
-}
