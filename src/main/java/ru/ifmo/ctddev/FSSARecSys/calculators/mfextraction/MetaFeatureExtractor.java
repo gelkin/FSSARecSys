aee0ca38bded3c0e6dda5d127fcdf2d18dd4fa37
Undin <arseniy1993@mail.ru>
2015-03-22 20:38:50 +0300

    added static method to create new instance of meta feature extractor from name

--- src/main/java/ru/ifmo/ctddev/FSSARecSys/calculators/mfextraction/MetaFeatureExtractor.java
+++ src/main/java/ru/ifmo/ctddev/FSSARecSys/calculators/mfextraction/MetaFeatureExtractor.java
@@ -3,10 +3,6 @@ package ru.ifmo.ctddev.FSSARecSys.calculators.mfextraction;
 import ru.ifmo.ctddev.FSSARecSys.db.DataSet;
 
 
-/**
- * some there here need to be created method that can return all Classes that implement ths interface(use Reflection library)
- * or it should be converted to abstract class?
- */
 public interface MetaFeatureExtractor {
     public String getName();
 
@@ -15,4 +11,31 @@ public interface MetaFeatureExtractor {
      * Any specific arguments?
      */
     public double extract(DataSet dataSet);
+
+    /**
+     * Creates a new instance of a meta feature extractor given it's class name.
+     *
+     * @param className the fully qualified class name of the meta feature extractor
+     * @return new instance of the meta feature extractor. If the extractor name is invalid return null
+     */
+    public static MetaFeatureExtractor forName(String className) {
+        Class<?> clazz;
+        try {
+            clazz = Class.forName(className);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+            return null;
+        }
+        if (!MetaFeatureExtractor.class.isAssignableFrom(clazz)) {
+            return null;
+        }
+        Object newInstance;
+        try {
+            newInstance = clazz.newInstance();
+        } catch (InstantiationException | IllegalAccessException e) {
+            e.printStackTrace();
+            return null;
+        }
+        return (MetaFeatureExtractor) newInstance;
+    }
 }
