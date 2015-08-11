5c8c8d1aa79ec3b18c4113c89e5db9500a38a5cc
seregamuravyov <mursmail@gmail.com>
2015-08-11 16:40:42 +0300

    fixed some bugs with EARR evaluation (still no db interaction)

--- src/main/java/ru/ifmo/ctddev/FSSARecSys/alternative/evaluation/classification/EARREvaluation.java
+++ src/main/java/ru/ifmo/ctddev/FSSARecSys/alternative/evaluation/classification/EARREvaluation.java
@@ -2,12 +2,16 @@ package ru.ifmo.ctddev.FSSARecSys.alternative.evaluation.classification;
 
 import ru.ifmo.ctddev.FSSARecSys.ClassifierResult;
 import ru.ifmo.ctddev.FSSARecSys.FSSResult;
-import ru.ifmo.ctddev.FSSARecSys.db.DataSet;
+import ru.ifmo.ctddev.FSSARecSys.db.internal.Dataset;
 import ru.ifmo.ctddev.FSSARecSys.db.internal.EARRParams;
 import ru.ifmo.ctddev.FSSARecSys.db.internal.FSSAlgorithm;
 import ru.ifmo.ctddev.FSSARecSys.db.internal.MLAlgorithm;
 import ru.ifmo.ctddev.FSSARecSys.db.internal.Metrics;
 import ru.ifmo.ctddev.FSSARecSys.db.manager.QueryManager;
+import weka.attributeSelection.ASEvaluation;
+import weka.attributeSelection.ASSearch;
+import weka.classifiers.Classifier;
+import weka.core.Instances;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,9 +26,7 @@ public class EARREvaluation {
     private float betta;
     private QueryManager queryManager;
 
-    public EARREvaluation(FSSResult fssResult, ClassifierResult classifierResult, float alpha, float betta, QueryManager queryManager) {
-        this.fssResult = fssResult;
-        this.classifierResult = classifierResult;
+    public EARREvaluation(float alpha, float betta, QueryManager queryManager) {
         this.alpha = alpha;
         this.betta = betta;
         this.queryManager = queryManager;
@@ -36,9 +38,41 @@ public class EARREvaluation {
                 betta * (params1.getNumberOfSelectedFeatures() / params2.getNumberOfSelectedFeatures()));
     }
 
-    public Double evaluate(FSSAlgorithm algo, ArrayList<FSSAlgorithm> allFSSAlgorithms, String datasetName, List<String> listOfFeatures, MLAlgorithm mlAlgorithm) {
+    public Double evaluate(FSSAlgorithm algo, ArrayList<FSSAlgorithm> allFSSAlgorithms, Dataset dataset, List<String> listOfFeatures, MLAlgorithm mlAlgorithm) throws Exception {
         Double sum = 0.0;
+        String datasetName = dataset.getName();
+
+        // if params for current fss algorithm is counted
         EARRParams newParams = queryManager.getEARRParams(new Metrics(algo, datasetName, listOfFeatures), mlAlgorithm);
+        if (newParams == null){
+
+            Instances datasetInstances = dataset.getInstances();
+
+            int trainSize = (int) Math.round(datasetInstances.numInstances() * 0.8);
+            int testSize = datasetInstances.numInstances() - trainSize;
+            Instances train = new Instances(datasetInstances, 0, trainSize);
+            Instances test = new Instances(datasetInstances, trainSize, testSize);
+
+            Classifier classifier = Classifier.forName(mlAlgorithm.getClassPath(), weka.core.Utils.splitOptions(mlAlgorithm.getOptions()));
+            ClassifierEvaluator classifierEvaluator = new ClassifierEvaluator(mlAlgorithm.getName(), classifier);
+            Double accuracy = classifierEvaluator.evaluate(mlAlgorithm.getName(), train, test).accuracy;
+
+            ASSearch asSearch = ASSearch.forName(algo.getSearchClass(), weka.core.Utils.splitOptions(algo.getSearchOptions()));
+            ASEvaluation asEvaluation = ASEvaluation.forName(algo.getEvalClass(), weka.core.Utils.splitOptions(algo.getEvalOptions()));
+
+            FSSClassificationEvaluator fssClassificationEvaluator = new FSSClassificationEvaluator(algo.getFssAlgoName(), asSearch, asEvaluation);
+            FSSResult fssResult = fssClassificationEvaluator.run(datasetName, datasetInstances);
+
+            Integer numberOfSelectedFeatures = fssResult.numberOfSelectedFeatures;
+            Long selectionTime = fssResult.selectionTime;
+
+            newParams = new EARRParams(accuracy, numberOfSelectedFeatures.doubleValue(), selectionTime.doubleValue());
+
+            queryManager.addMetricParam(new Metrics(algo, datasetName, listOfFeatures), "accuracy", mlAlgorithm, accuracy);
+            queryManager.addMetricParam(new Metrics(algo, datasetName, listOfFeatures), "number of selected features", mlAlgorithm, numberOfSelectedFeatures.doubleValue());
+            queryManager.addMetricParam(new Metrics(algo, datasetName, listOfFeatures), "selection time", mlAlgorithm, selectionTime.doubleValue());
+        }
+
         for (FSSAlgorithm fssAlgorithm : allFSSAlgorithms) {
             if (fssAlgorithm != algo) {
                 EARRParams otherParams = queryManager.getEARRParams(new Metrics(fssAlgorithm, datasetName, listOfFeatures), mlAlgorithm);
