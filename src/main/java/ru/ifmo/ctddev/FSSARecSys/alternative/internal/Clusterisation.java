5e4787ac81578056f6e2cee2363b2f243586e30d
seregamuravyov <mursmail@gmail.com>
2015-08-21 15:09:41 +0300

    all clustering metrics done (not tested)

--- src/main/java/ru/ifmo/ctddev/FSSARecSys/alternative/internal/Clusterisation.java
+++ src/main/java/ru/ifmo/ctddev/FSSARecSys/alternative/internal/Clusterisation.java
@@ -75,6 +75,8 @@ public class Clusterisation {
         return all;
     }
 
+    // ** Dunn's index **
+
     public Double DunnIndex(){
 
         //get max inter-cluster distance
@@ -102,14 +104,32 @@ public class Clusterisation {
         //get min intra-cluster distance
 
         boolean [][] intraClusterMatrix = new boolean[numOfClusters][numOfClusters];
-        
+
+        Double minIntraclusterDistance = Double.MAX_VALUE;
+        Double minLocalDistance = Double.MAX_VALUE;
         for (int i = 0; i < numOfClusters; i++) {
             for (int j = 0; j < numOfClusters; j++) {
+                if (intraClusterMatrix[j][i] == false) {
+                    intraClusterMatrix[i][j] = true;
+                    Instances clusterI = clusters.get(i);
+                    Instances clusterJ = clusters.get(j);
 
+                    minLocalDistance = Double.MAX_VALUE;
+                    for (int k = 0; k < clusterI.numInstances(); k++) {
+                        for (int p = 0; p < clusterJ.numInstances(); p++) {
+                            Instance first = clusterI.instance(k);
+                            Instance second = clusterJ.instance(p);
+                            minLocalDistance = Double.min(minLocalDistance, allInstancedDist.distance(first, second));
+                        }
+                    }
+                }
             }
+            minIntraclusterDistance = Double.min(minIntraclusterDistance, minLocalDistance);
         }
-        return 0.0;
+        return minIntraclusterDistance;
     }
+
+    // ** Davies-Bouldin index **
 
     public Double DaviesBouldinIndex() throws Exception {
         //count S_i = 1 / |C_i| sum_all_xi(dist(x_i, centr_i))
@@ -138,8 +158,9 @@ public class Clusterisation {
 
         EuclideanDistance centroidDist = new EuclideanDistance(centroids);
 
+        Double maxVal = Double.MIN_VALUE;
         for (int i = 0; i < clusters.size(); i++) {
-            Double maxVal = Double.MIN_VALUE;
+            maxVal = Double.MIN_VALUE;
             for (int j = 0; j < clusters.size(); j++)
                 if (i != j) {
                     maxVal = Double.max(maxVal, (sTemp.get(i) + sTemp.get(j)) /
@@ -157,9 +178,55 @@ public class Clusterisation {
         return result / numOfClusters;
     }
 
+    // ** silhouette index **
+
+    private Double avgInterClusterDistance(int instanceIndex, Instances cluster) {
+        Instance current = cluster.instance(instanceIndex);
+        EuclideanDistance ed = new EuclideanDistance(cluster);
+        Double result = 0.0;
+        for (int i = 0; i < cluster.numInstances(); i++) {
+            if (instanceIndex != i)
+                result += ed.distance(current, cluster.instance(i));
+        }
+        return result / cluster.numInstances();
+    }
+
+    private Double avgIntraClusterDistace(int clusterIndex, Instance x) {
+        EuclideanDistance ed = new EuclideanDistance(unitedClusters);
+
+        Double avgDistance = 0.0;
+        Double minVal = Double.MAX_VALUE;
+        for (int i = 0; i < clusters.size(); i++) {
+            avgDistance = 0.0;
+            if (i != clusterIndex) {
+                Instances currentCluster = clusters.get(i);
+                for (int j = 0; j < currentCluster.numInstances(); j++) {
+                    Instance currentInstance = currentCluster.instance(j);
+                    avgDistance += ed.distance(x, currentInstance);
+                }
+                minVal = Double.min(minVal, avgDistance);
+            }
+        }
+        return minVal;
+    }
+
+
     public Double silhouetteIndex(){
 
-        return 0.0;
+        Double result = 0.0;
+        Double silhoetteOfInstanse = 0.0;
+        for (int i = 0; i < numOfClusters; i++) {
+            Instances currentCluster = clusters.get(i);
+            silhoetteOfInstanse = 0.0;
+            for (int j = 0; j < currentCluster.numInstances(); j++) {
+                Double a = avgInterClusterDistance(j, currentCluster);
+                Double b = avgIntraClusterDistace(i, currentCluster.instance(j));
+                silhoetteOfInstanse += (b - a) / Double.max(a, b);
+            }
+            result += silhoetteOfInstanse / currentCluster.numInstances();
+        }
+
+        return result / numOfClusters;
     }
 
 }
