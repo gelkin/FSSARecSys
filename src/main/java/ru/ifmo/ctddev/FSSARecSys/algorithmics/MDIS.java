bb1ad16e947c4e8da508a6f8f14ccab3907f8fc8
gelkin <mazin.v.a@gmail.com>
2016-04-18 17:19:59 +0300

    Adding KernelEstimator for uniform density estimation

--- src/main/java/ru/ifmo/ctddev/FSSARecSys/algorithmics/MDIS.java
+++ src/main/java/ru/ifmo/ctddev/FSSARecSys/algorithmics/MDIS.java
@@ -9,6 +9,7 @@ import ru.ifmo.ctddev.FSSARecSys.db.internal.FSSAlgorithm;
 import ru.ifmo.ctddev.FSSARecSys.db.internal.MLAlgorithm;
 import ru.ifmo.ctddev.FSSARecSys.db.manager.QueryManager;
 import ru.ifmo.ctddev.FSSARecSys.utils.Pair;
+import weka.estimators.KernelEstimator;
 
 import java.util.*;
 import java.util.stream.Collectors;
@@ -32,15 +33,17 @@ public class MDIS {
     private ArrayList<Dataset> dataSets;
     private ArrayList<Integer> bestFSSAlgorithms; // indexes of best FSSAlgorithms for given datasets
 
-    private double[][] Q;
+    private double[][] Q; // table Q[algorithm_ind][dataset_ind] = EARR
     private MLAlgorithm mlAlgo;
     private QueryManager queryManager;
 
-    private Set<Integer> completeDatasets = new HashSet<>(); // set of dataset with complete Q table
-    private Set<Integer> incompleteDatasets = new HashSet<>(); // all datasets without completeDatasets
+    private Set<Integer> completeDatasets = new HashSet<>(); // set of (indexes of) datasets with complete Q table
+    private Set<Integer> incompleteDatasets = new HashSet<>(); // all (indexes of) datasets without completeDatasets
 
     private static final double AMB_COEF = 2.0; // tunable multiplier of |C i−1 | depends on how ambitious one
                                                 // wants to be especially early on when |C i−1 | is not too large
+
+    private static final double PRECISION = 0.01; // precision for density estimation
 
     public MDIS(ArrayList<FSSAlgorithm> algorithms,
                 ArrayList<Dataset> dataSets,
@@ -65,7 +68,7 @@ public class MDIS {
         datasetLoop:
         for (int i = 0; i < Q[0].length; ++i) {
             for (int j = 0; j < Q.length; ++i) {
-                if (Q[j][i] == 0.0) {
+                if (isValueMissing(Q[j][i])) {
                     incompleteDatasets.add(i);
                     continue datasetLoop;
                 }
@@ -78,7 +81,7 @@ public class MDIS {
      * TODO
      *
      * @param recSystem - recommendation system for first iteration (step)
-     * @param k - number of instances to query
+     * @param k - number of instances to acquire
      * @param g - number of iterations (steps)
      * @return Pair:
      * 1. First element of pair: list of set of dataset indexes. Each set contains
@@ -108,40 +111,80 @@ public class MDIS {
         // Asymmetric uncertainty:
         double b = Math.min(0.5 + k / (AMB_COEF * completeDatasets.size()), 1.0);
         for (Integer i : incompleteDatasets) {
-            double p = getProbability(recSystem, i, algorithms.get(bestFSSAlgorithms.get(i)));
+            double p = getProbability(recSystem, i, bestFSSAlgorithms.get(i));
             double score = p * (1 - p) / ((-2 * b + 1) * p + Math.pow(b, 2.0));
             scores.add(new Pair<>(score, i));
         }
 
-        // Choose best 'k' datasets to query:
+        // Choose best 'k' datasets to acquire:
         Collections.sort(scores);
-        ArrayList<Integer> datasetsToQuery = scores.subList(0, k).stream()
+        ArrayList<Integer> bestDatasets = scores.subList(0, k).stream()
                 .map(pair -> pair.second)
                 .collect(Collectors.toCollection(ArrayList::new));
 
-        for (Integer i : datasetsToQuery) {
-            queryDataset(i);
+        for (Integer i : bestDatasets) {
+            acquireDataset(i);
         }
 
-        for (Integer i : datasetsToQuery) {
+        for (Integer i : bestDatasets) {
             completeDatasets.add(i);
             incompleteDatasets.remove(i);
         }
 
 
-        return new Pair<>(datasetsToQuery, RecommendationSystemBuilder.makeRecommendationSystem(algorithms, dataSets, Q, mlAlgo));
+        return new Pair<>(bestDatasets, RecommendationSystemBuilder.makeRecommendationSystem(algorithms, dataSets, Q, mlAlgo));
     }
 
-    private double getProbability(RecommendationSystem recSystem, int datasetIndex, FSSAlgorithm resAlgo) {
+    private double getProbability(RecommendationSystem recSystem, int datasetIndex, int resAlgoIndex) {
         List<Integer> missingCells = getMissingCells(datasetIndex);
-        // TODO
-        return 0.0;
+
+        double[] curDataset = new double[Q.length];
+        // Fill not missing features for curDataset
+        for (int i = 0; i < Q.length; ++i) {
+            if (Q[i][datasetIndex] != 0.0) {
+                curDataset[i] = Q[i][datasetIndex];
+            }
+        }
+
+        // Univariate density estimator
+        KernelEstimator estimator = new KernelEstimator(PRECISION);
+
+        // Count all complete
+        for (Integer datasetId : completeDatasets) {
+            for (Integer cellId : missingCells) {
+                curDataset[cellId] = Q[cellId][datasetId];
+            }
+
+            int label = getLabelOnEARRs(curDataset);
+            estimator.addValue((double) label, 1.0); // TODO should I normalize labels from 0 to 1, which weight to choose? (if not 1.0)
+        }
+
+        // Count incomplete
+        for (Integer datasetId : incompleteDatasets) {
+            boolean isDatasetMatched = true;
+            for (Integer cellId : missingCells) {
+                if (isValueMissing(Q[cellId][datasetId])) {
+                    isDatasetMatched = false;
+                    break;
+                }
+            }
+            if (isDatasetMatched) {
+                for (Integer cellId : missingCells) {
+                    curDataset[cellId] = Q[cellId][datasetId];
+                }
+
+                int label = getLabelOnEARRs(curDataset);
+                estimator.addValue((double) label, 1.0); // TODO should I normalize labels from 0 to 1, which weight to choose? (if not 1.0)
+            }
+        }
+
+        return 1 - estimator.getProbability(resAlgoIndex); // TODO prob( xi(dataset) != resAlgo )
     }
 
     private List<Integer> getMissingCells(int datasetIndex) {
         List<Integer> missingCells = new ArrayList<>();
         for (int i = 0; i < Q.length; ++i) {
-            if (Q[i][datasetIndex] == 0.0) {
+            if (isValueMissing(Q[i][datasetIndex])) {
                 missingCells.add(i);
             }
         }
@@ -149,15 +192,36 @@ public class MDIS {
         return missingCells;
     }
 
-    private void queryDataset(int datasetIndex) throws Exception { // todo throw Exception?
+    private void acquireDataset(int datasetIndex) throws Exception {
         for (int i = 0; i < Q.length; ++i) {
-            if (Q[i][datasetIndex] == 0.0) {
+            if (isValueMissing(Q[i][datasetIndex])) {
                 Evaluator newEvaluator = new Evaluator(queryManager);
                 Q[i][datasetIndex] = newEvaluator.evaluate(algorithms.get(i).toDbObject(),
                         dataSets.get(datasetIndex),
                         mlAlgo);
             }
         }
+    }
+
+    // TODO: пока не очень понятно, как оценивать результат классификатора
+    // TODO: будем брать алгоритм с максимальным EARR
+    // TODO: считаем, что индекс fssAlgorithm'а - это и есть label
+    private int getLabelOnEARRs(double[] datasetEARRs) {
+        double curMax = Double.MIN_VALUE;
+        int fssAlgoIndex = -1;
+        for (int i = 0; i < datasetEARRs.length; ++i) {
+            if (datasetEARRs[i] > curMax) {
+                curMax = datasetEARRs[i];
+                fssAlgoIndex = i;
+            }
+        }
+
+        return fssAlgoIndex;
+    }
+
+
+    public static boolean isValueMissing(double value) {
+        return value == 0.0;
     }
 }
 
